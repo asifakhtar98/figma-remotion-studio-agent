26f455aed1e94458d66e4c19bd4737160f9774f3
asak98 <asifakhtar91298.personal@gmail.com>
2026-08-12 01:54:32 +0530

    fix(vhims): HomeScreen — pass 2 visual refinement with Inter Google font and strict canvas layout

--- src/projects/vhims/src/screens/HomeScreen.tsx
+++ src/projects/vhims/src/screens/HomeScreen.tsx
@@ -1,27 +1,36 @@
 import React from 'react';
+import { loadFont } from '@remotion/google-fonts/Inter';
 import { HomeHeader } from '../components/HomeHeader';
 import { HomeHero } from '../components/HomeHero';
 import { ThreePillars } from '../components/ThreePillars';
 import { ValueCards } from '../components/ValueCards';
 import { HomeFooter } from '../components/HomeFooter';
 
+const { fontFamily } = loadFont();
+
 export const HomeScreen: React.FC = () => {
   return (
-    <div className="w-[1920px] h-[2800px] bg-white font-sans antialiased text-gray-900 select-none overflow-y-auto flex flex-col">
-      {/* Header Bar */}
-      <HomeHeader />
+    <div
+      style={{ fontFamily }}
+      className="w-[1920px] min-h-[2800px] bg-white antialiased text-slate-800 select-none flex flex-col justify-between"
+    >
+      <div>
+        {/* Header Bar */}
+        <HomeHeader />
 
-      {/* Hero Banner */}
-      <HomeHero />
+        {/* Hero Banner */}
+        <HomeHero />
 
-      {/* Three Pillars Section */}
-      <ThreePillars />
+        {/* Three Pillars Section */}
+        <ThreePillars />
 
-      {/* Value You Can Feel Section */}
-      <ValueCards />
+        {/* Value You Can Feel Section */}
+        <ValueCards />
+      </div>
 
       {/* Footer */}
       <HomeFooter />
     </div>
   );
 };
+
